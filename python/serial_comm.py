ee05c0d7f270f7049221eabc18b0a08b6b943a33
novast1024 <167774662+novast1024@users.noreply.github.com>
2026-01-10 23:48:10 +0900

    Delete python/serial_comm.py

--- python/serial_comm.py
+++
@@ -1,9 +0,0 @@
-import serial
-from codec import cobsr, crc8
-
-class SerialHandle:
-    def __init__(self) -> None:
-        self.serial = serial.Serial("COM6", 115200)
-
-    def send(self, data: bytes):
-        self.serial.write(cobsr.encode(data + int.to_bytes(crc8.ccitt(data))) + b"\x00")
