c5a2e6b1296afde8676669041ef3cde6fb423ae8
novast1024 <167774662+novast1024@users.noreply.github.com>
2026-01-10 17:48:10 +0900

    Add files via upload

---
+++ pyscript/model/__init__.py
@@ -0,0 +1,5 @@
+from .vector import Vec2
+from .intflag import Button, HatSwitch
+from .gamepad import Gamepad
+
+__all__ = ["Vec2", "Button", "HatSwitch", "Gamepad"]
---
+++ pyscript/test_gamepad.py
@@ -0,0 +1,34 @@
+import time
+import hid # pip install hidapi
+import serial
+
+from model import Gamepad
+from codec import cobsr, crc8
+
+ser = serial.Serial("COM6", 115200)
+
+dev_dict_list = hid.enumerate()
+dev_dict_list.sort(key=lambda it: it["product_string"])
+for i, dev_dict in enumerate(dev_dict_list):
+    print(f"{i} vid=0x{dev_dict["vendor_id"]:04x} pid=0x{dev_dict["product_id"]:04x} {dev_dict["product_string"]}")
+
+try:
+    idx = int(input(f"Select a device (0-{len(dev_dict_list)-1}): "))
+    dev_dict = dev_dict_list[idx]
+    device = hid.device()
+    print(f"Opening the {dev_dict["product_string"]}")
+    device.open(dev_dict["vendor_id"], dev_dict["product_id"])
+    device.set_nonblocking(1)
+
+    seconds = float(input("Enter seconds: "))
+    start = time.perf_counter()
+    while time.perf_counter() - start < seconds:
+        if report := device.read(64):
+            data = bytes(report)
+            print(Gamepad.from_hid_report(data))
+            ser.write(cobsr.encode(data + int.to_bytes(crc8.ccitt(data))) + b"\x00")
+    print(f"Closing the {dev_dict["product_string"]}")
+    device.close()
+
+except (ValueError, IndexError, IOError) as e:
+    print(e)
